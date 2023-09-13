8d4d515fc72101816fb2a91d9741cf2192e9d0d5
Ryan Bonial <ryanbonial@netlify.com>
2023-09-13 16:33:55 -0600

    Add dev command

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,6 +4,7 @@ import { GitContentSource } from '@stackbit/cms-git'
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'custom',
+  devCommand: 'npm run dev',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
