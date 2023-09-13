7dc62471547e673e9924883e2303dbaf95fdd26f
Ryan Bonial <ryanbonial@netlify.com>
2023-09-13 16:25:50 -0600

    Add ssg

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,6 +3,7 @@ import { GitContentSource } from '@stackbit/cms-git'
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
+  ssgName: 'custom',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
