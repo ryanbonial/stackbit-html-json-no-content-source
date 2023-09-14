43fc4f7a1c9a15622ef6decc6b0bb6659310c9e5
Ryan Bonial <4294665+ryanbonial@users.noreply.github.com>
2023-09-14 08:37:50 -0600

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,7 +4,7 @@ import { GitContentSource } from '@stackbit/cms-git'
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'custom',
-  nodeVersion: '16',
+  nodeVersion: '18',
   devCommand: 'npm run dev',  
   contentSources: [
     new GitContentSource({
