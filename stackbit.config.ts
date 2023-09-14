ebe5bf3a557018600aaca67ec6c34714796bbf96
Ryan Bonial <ryanbonial@netlify.com>
2023-09-14 10:08:12 -0600

    back to node 18

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,8 +4,8 @@ import { GitContentSource } from '@stackbit/cms-git'
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'custom',
-  nodeVersion: '20',
-  devCommand: 'npm run dev',  
+  nodeVersion: '18',
+  devCommand: 'npm run dev',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
